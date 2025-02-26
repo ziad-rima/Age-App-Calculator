8a063d68a925f8a55df32f1e152644525c6171ef
Ziad Rima <ziadrima49@gmail.com>
2025-02-26 11:20:30 +0100

    create the button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import ArrowIconSVG from "./ArrowIconSVG"
+
+const Button = () => {
+  return (
+    <div className='button-component'>
+      <button className='age-button'><ArrowIconSVG /></button>
+      <div className="divider"></div>
+    </div>
+  )
+}
+
+export default Button 
