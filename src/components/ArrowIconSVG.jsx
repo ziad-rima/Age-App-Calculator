9f03c9e73076473a5b1f3591b9433595d166c78c
Ziad Rima <ziadrima49@gmail.com>
2025-02-26 11:20:50 +0100

    create svg for button

---
+++ src/components/ArrowIconSVG.jsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+const ArrowIconSVG = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={30}
+    height={28}
+    viewBox="0 0 46 44"
+    {...props}
+  >
+    <g fill="none" stroke="#FFF" strokeWidth={2}>
+      <path d="M1 22.019C8.333 21.686 23 25.616 23 44M23 44V0M45 22.019C37.667 21.686 23 25.616 23 44" />
+    </g>
+  </svg>
+);
+export default ArrowIconSVG;
